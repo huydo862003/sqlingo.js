2a35654539b1b8f406aac3c3c0b1a12e19ed6557
Huy-DNA <huydo862003@gmail.com>
2026-02-07 11:55:22 +0700

    feat: port trie.py to ts

--- src/trie.ts
+++ src/trie.ts
@@ -1,15 +1,51 @@
-export class TrieNode {
-  // Placeholder
+// https://github.com/tobymao/sqlglot/blob/264e95f04d95f2cd7bcf255ee7ae160db36882a7/sqlglot/trie.py
+
+export const enum TrieResult {
+  FAILED,
+  PREFIX,
+  EXISTS,
 }
 
-export class Trie {
-  root: TrieNode = new TrieNode();
+export type TrieNode = { [key: string]: TrieNode } & { 0?: true };
 
-  insert (_word: string): void {
-    throw new Error('Trie.insert not implemented');
+export type Key = string[];
+
+export function newTrie (keywords: Key[], trie?: TrieNode): TrieNode {
+  const result = trie ?? {};
+
+  for (const key of keywords) {
+    let current: TrieNode = result;
+    for (const char of key) {
+      if (!current[char]) {
+        current[char] = {};
+      }
+      current = current[char];
+    }
+
+    current[0] = true;
   }
 
-  search (_word: string): boolean {
-    throw new Error('Trie.search not implemented');
+  return result;
+}
+
+export function inTrie (trie: TrieNode, key: Key): [TrieResult, TrieNode] {
+  const keyArray = Array.from(key);
+
+  if (!keyArray.length) {
+    return [TrieResult.FAILED, trie];
   }
+
+  let current = trie;
+  for (const char of keyArray) {
+    if (!current[char]) {
+      return [TrieResult.FAILED, current];
+    }
+    current = current[char];
+  }
+
+  if (current[0]) {
+    return [TrieResult.EXISTS, current];
+  }
+
+  return [TrieResult.PREFIX, current];
 }
