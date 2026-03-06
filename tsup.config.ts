2f95403b12f085bdfcc370be97ce5f436e702b8e
Huy-DNA <huydo862003@gmail.com>
2026-03-06 20:11:16 +0700

    chore: use tsup

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  sourcemap: true,
+  minify: true,
+  external: ['luxon'],
+  clean: true,
+  splitting: true,
+  outDir: 'dist',
+});
