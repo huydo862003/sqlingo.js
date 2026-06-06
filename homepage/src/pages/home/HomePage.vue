fb420e14bfca4c488d201d55bc7e362bba10c5c3
Huy-DNA <huydo862003@gmail.com>
2026-06-06 22:09:45 +0700

    doc: bump referenced version to 0.3.0

--- homepage/src/pages/home/HomePage.vue
+++ homepage/src/pages/home/HomePage.vue
@@ -45,7 +45,7 @@
               :color="GPillColor.Orange"
               :size="GBadgeSize.Md"
               :badge-style="GBadgeStyle.Flat"
-              value="v0.2.2"
+              value="v0.3.0"
               href="https://www.npmjs.com/package/@hdnax/sqlingo.js"
             />
             <GBadge
@@ -238,7 +238,7 @@
           "applicationCategory": "DeveloperApplication",
           "operatingSystem": "All",
           "license": "https://opensource.org/licenses/MIT",
-          "softwareVersion": "0.2.2",
+          "softwareVersion": "0.3.0",
           "url": "https://huydo862003.github.io/sqlingo.js/",
           "author": {
             "@type": "Person",
